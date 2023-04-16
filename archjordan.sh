5ac1f46559a7838bf12e79bbbe0f4ee2dc3f4fcc
frickenhek <125307966+frickenhek@users.noreply.github.com>
2023-04-15 23:16:46 -0500

    Update archjordan.sh

--- archjordan.sh
+++ archjordan.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #github-action genshdoc
 #
-# @file ArchTitus
+# @file ArchJordan
 # @brief Entrance script that launches children scripts for each phase of installation.
 
 # Find the name of the folder the scripts are in
@@ -26,11 +26,11 @@ echo -ne "
     ( bash $SCRIPT_DIR/scripts/startup.sh )|& tee startup.log
       source $CONFIGS_DIR/setup.conf
     ( bash $SCRIPT_DIR/scripts/0-preinstall.sh )|& tee 0-preinstall.log
-    ( arch-chroot /mnt $HOME/ArchTitus/scripts/1-setup.sh )|& tee 1-setup.log
+    ( arch-chroot /mnt $HOME/ArchJordan/scripts/1-setup.sh )|& tee 1-setup.log
     if [[ ! $DESKTOP_ENV == server ]]; then
-      ( arch-chroot /mnt /usr/bin/runuser -u $USERNAME -- /home/$USERNAME/ArchTitus/scripts/2-user.sh )|& tee 2-user.log
+      ( arch-chroot /mnt /usr/bin/runuser -u $USERNAME -- /home/$USERNAME/ArchJordan/scripts/2-user.sh )|& tee 2-user.log
     fi
-    ( arch-chroot /mnt $HOME/ArchTitus/scripts/3-post-setup.sh )|& tee 3-post-setup.log
+    ( arch-chroot /mnt $HOME/ArchJordan/scripts/3-post-setup.sh )|& tee 3-post-setup.log
     cp -v *.log /mnt/home/$USERNAME
 
 echo -ne "
